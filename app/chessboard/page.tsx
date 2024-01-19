b5e6b3a18f9c3cdd319570429c204a7d07c42212
Kislay <88523649+kuv2707@users.noreply.github.com>
2024-01-20 02:17:52 +0530

    fetching moves from rust backend

--- app/chessboard/page.tsx
+++ app/chessboard/page.tsx
@@ -1,7 +1,40 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
+
+type PieceMoves = {
+	[index: string]: string[];
+};
+
+function fenString(board: string[][], turn: string) {
+	let fen = "";
+	for (let i = 0; i < 8; i++) {
+		let empty = 0;
+		for (let j = 0; j < 8; j++) {
+			if (board[i][j] === "") {
+				empty++;
+			} else {
+				if (empty != 0) {
+					fen += empty;
+					empty = 0;
+				}
+				fen += board[i][j];
+			}
+		}
+		if (empty != 0) {
+			fen += empty;
+			empty = 0;
+		}
+		if (i != 7) fen += "/";
+	}
+	if (turn === "white") {
+		fen += " w";
+	} else {
+		fen += " b";
+	}
+	return fen;
+}
 
 export default function ChessBoard() {
 	//replace with api call
@@ -17,33 +50,68 @@ export default function ChessBoard() {
 	]);
 	const [turn, setTurn] = useState("white");
 	const [selectedSquare, setSelectedSquare] = useState<number>(-1);
-	const [selectedPieceMoves, setSelectedPieceMoves] = useState([]);
+	const [allPieceMoves, setAllPieceMoves] = useState<PieceMoves>({});
+	const [selectedPieceDests, setSelectedPieceDests] = useState<number[]>([]);
 	const [check, setCheck] = useState(false);
 	const [checkMate, setCheckMate] = useState(false);
 	const [winner, setWinner] = useState(null);
 	const [lastMove, setLastMove] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:8000/piecewisemoves", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ fen: fenString(board, turn) }),
+		})
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setAllPieceMoves(data);
+			});
+	}, [board, turn]);
+
 	function squareColor(index: number) {
+		if (selectedPieceDests.includes(index)) {
+			return "bg-green-500";
+		}
 		if (selectedSquare == index) {
 			return "bg-green-500";
 		}
 		return (index + Math.floor(index / 8)) % 2 === 0
-			? "bg-white"
-			: "bg-gray-300";
+			? "bg-amber-200"
+			: "bg-gray-800";
 	}
 	function handleSelectSquare(index: number) {
 		if (selectedSquare != -1) {
-			setBoard((board) => {
-				let newboard = JSON.parse(JSON.stringify(board));
-				newboard[Math.floor(selectedSquare / 8)][selectedSquare % 8] =
-					"";
-				newboard[Math.floor(index / 8)][index % 8] =
-					board[Math.floor(selectedSquare / 8)][selectedSquare % 8];
-				return newboard;
-			});
+			if (selectedPieceDests.includes(index)) {
+				setBoard((board) => {
+					let newboard = JSON.parse(JSON.stringify(board));
+					newboard[Math.floor(selectedSquare / 8)][
+						selectedSquare % 8
+					] = "";
+					newboard[Math.floor(index / 8)][index % 8] =
+						board[Math.floor(selectedSquare / 8)][
+							selectedSquare % 8
+						];
+					return newboard;
+				});
+				setTurn(turn === "white" ? "black" : "white");
+			}
 			setSelectedSquare(-1);
+			setSelectedPieceDests([]);
 		} else {
-            if(board[Math.floor(index / 8)][index % 8] === "") return;
+			if (
+				board[Math.floor(index / 8)][index % 8] === "" ||
+				!allPieceMoves[index]
+			)
+				return;
 			setSelectedSquare(index);
+			setSelectedPieceDests(
+				allPieceMoves[index].map((k) => k.substring(2)).map(encodePos)
+			);
 		}
 	}
 	function fileName(piece: string) {
@@ -63,9 +131,16 @@ export default function ChessBoard() {
 					key={index}
 					className={`w-12 h-12 ${squareColor(index)}`}
 					onClick={() => handleSelectSquare(index)}
+					onMouseEnter={(e: React.MouseEvent<HTMLDivElement>) =>
+						(e.currentTarget.style.filter = "brightness(1.5)")
+					}
+					onMouseLeave={(e: React.MouseEvent<HTMLDivElement>) =>
+						(e.currentTarget.style.filter = "brightness(1)")
+					}
 				>
 					{piece && (
 						<Image
+							draggable={false}
 							src={"/images/pieces/" + fileName(piece)}
 							alt={piece}
 							width={50}
@@ -76,4 +151,10 @@ export default function ChessBoard() {
 			))}
 		</div>
 	);
+}
+
+function encodePos(pos: string) {
+	let x = pos.charCodeAt(0) - 97;
+	let y = 8 - parseInt(pos[1]);
+	return x + y * 8;
 }
